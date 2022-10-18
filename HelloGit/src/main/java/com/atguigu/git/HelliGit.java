740b7a75cbf556964d9ddb7dfe7fddd7e16523a8
张锦阳 <1146705500@qq.com>
2022-10-19 00:08:37 +0800

    更新1

--- HelloGit/src/main/java/com/atguigu/git/HelliGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelliGit.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class HelliGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("更新1!");
     }
 }
